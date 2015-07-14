16b440baea579ff86e7103b4396f9ad7c2920f0d
Deamor <deamor76@gmail.com>
2015-07-14 13:15:58 +0600

    Add users table into database

--- src/main/resources/db/migration/V2015.07.07.19.39__Create_discounts_table.sql
+++ src/main/resources/db/migration/V2015.07.07.19.39__Create_discounts_table.sql
@@ -1,7 +1,20 @@
 CREATE TABLE discounts (
   "id" BIGSERIAL NOT NULL PRIMARY KEY,
-  "key" VARCHAR(4) NOT NULL,
-  "uin" VARCHAR(4) NOT NULL,
+  "key" VARCHAR(8) NOT NULL,
+  "uin" VARCHAR(8) NOT NULL,
   "is_hidden" BOOLEAN NOT NULL,
-  "user_id" VARCHAR(4) NOT NULL
+  "user_id" VARCHAR(8) NOT NULL
 );
+
+CREATE TABLE users (
+  "id" BIGSERIAL NOT NULL PRIMARY KEY,
+  "email" VARCHAR(255) NOT NULL,
+  "user_id" VARCHAR(8) NOT NULL,
+  "password" VARCHAR(255) NOT NULL,
+  "is_store" BOOLEAN NOT NULL
+);
+
+INSERT INTO users("id","email","user_id","password","is_store")
+values
+  (1,'user@user.user','user','user',false),
+  (2,'store@store.store','store','store',true);
