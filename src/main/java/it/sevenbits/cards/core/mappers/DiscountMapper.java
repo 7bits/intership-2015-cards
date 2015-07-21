51b9a0e548a0baef019ef16c1f89872533e862f5
Kuzmin Dmitry <kuzmindmitry1994@gmail.com>
2015-07-21 16:27:17 +0600

    Refactoring, validation for send discount function, authorization and support session, security config fix, validation for show discounts in personal area via backend

--- src/main/java/it/sevenbits/cards/core/mappers/DiscountMapper.java
+++ src/main/java/it/sevenbits/cards/core/mappers/DiscountMapper.java
@@ -18,14 +18,14 @@ public interface DiscountMapper {
             @Result(column = "description", property = "description")
     })
     List<Discount> findAll();
-    //save
+    //Save
     @Insert("INSERT INTO discounts (key, uin, is_hidden, user_id, store_name, description) VALUES (#{key}, #{uin}, #{isHidden}, #{userId}, #{storeName}, #{description})")
     void save(final Discount discount);
-    //delete
+    //Delete
     @Delete("DELETE FROM discounts WHERE uin = #{uin}")
     void delete(final Discount discount);
-    //findAllDiscountsToUse
-    @Select("SELECT id, key, uin, is_hidden, user_id, store_name, description FROM discounts WHERE is_hidden = false")
+    //FindAllForUse
+    @Select("SELECT id, key, uin, is_hidden, user_id, store_name, description FROM discounts WHERE is_hidden = false and user_id = #{userName}")
     @Results({
             @Result(column = "id", property = "id"),
             @Result(column = "key", property = "key"),
@@ -35,9 +35,9 @@ public interface DiscountMapper {
             @Result(column = "store_name", property = "storeName"),
             @Result(column = "description", property = "description")
     })
-    List<Discount> findAllDiscountsToUse();
-    //findAllDiscountsToSend
-    @Select("SELECT id, key, uin, is_hidden, user_id, store_name, description FROM discounts WHERE is_hidden = true")
+    List<Discount> findAllForUse(@Param("userName") String userName);
+    //FindAllForSend
+    @Select("SELECT id, key, uin, is_hidden, user_id, store_name, description FROM discounts WHERE is_hidden = true and user_id = #{userName}")
     @Results({
             @Result(column = "id", property = "id"),
             @Result(column = "key", property = "key"),
@@ -47,18 +47,8 @@ public interface DiscountMapper {
             @Result(column = "store_name", property = "storeName"),
             @Result(column = "description", property = "description")
     })
-    List<Discount> findAllDiscountsToSend();
-    //findRegisteredUsers
-    @Select("SELECT id, email, user_id, password, is_store FROM users WHERE email = #{email} AND  password = #{password}")
-    @Results({
-            @Result(column = "id", property = "id"),
-            @Result(column = "email", property = "email"),
-            @Result(column = "user_id", property = "userId"),
-            @Result(column = "password", property = "password"),
-            @Result(column = "is_store", property = "isStore")
-    })
-    boolean findRegisteredUsers(final User user);
-    //findUserId
+    List<Discount> findAllForSend(@Param("userName") String userName);
+    //FindUserId
     @Select("SELECT id, key, uin, is_hidden, user_id, store_name, description FROM discounts WHERE uin = #{uin}")
     @Results({
             @Result(column = "id", property = "id"),
@@ -70,6 +60,8 @@ public interface DiscountMapper {
             @Result(column = "description", property = "description")
     })
     List<Discount> findUserId(final Discount discount);
+    //ChangeUserId
     @Update("UPDATE discounts SET user_id = 'user' WHERE uin = #{uin}") void changeUserId(final Discount discount);
-    @Update("UPDATE discounts SET user_id = #{userId} WHERE uin = #{uin}") void sendDiscount(final Discount discount);
+    //Send
+    @Update("UPDATE discounts SET user_id = #{userId} WHERE uin = #{uin}") void send(@Param("userId") String userId,@Param("uin") String uin,@Param("userName") String userName);
 }
--- src/main/java/it/sevenbits/cards/web/controllers/HomeController.java
+++ src/main/java/it/sevenbits/cards/web/controllers/HomeController.java
@@ -3,11 +3,15 @@ import it.sevenbits.cards.web.service.DiscountService;
 import it.sevenbits.cards.web.service.ServiceException;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import javax.servlet.http.HttpServletRequest;
+import java.net.Authenticator;
 
 @Controller
 public class HomeController {
@@ -15,40 +19,47 @@ public class HomeController {
     private DiscountService discountService;
     private Logger LOG = Logger.getLogger(HomeController.class);
 
-    //Homepage from root
-    //Aloha
-    //Index page
+    //Login
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String getLogin(HttpServletRequest request) {
-        return "home/login";
+        return "redirect:/homepage";
     }
 
+    //Homepage
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String root() {
         return "home/homepage";
     }
+
     //Homepage
     @RequestMapping(value = "/homepage", method = RequestMethod.GET)
     public String homepage(final Model model) {
         return "home/homepage";
     }
+
     //Personal Area Get Method
     @RequestMapping(value = "/personal_area", method = RequestMethod.GET)
     public String personalArea(final Model model) throws ServiceException {
-        model.addAttribute("discounts", discountService.findAll());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName();
+        model.addAttribute("userName", userName);
+        model.addAttribute("discountsForUse", discountService.findAllForUse(userName));
+        model.addAttribute("discountsForSend", discountService.findAllForSend(userName));
         return "home/personal_area";
     }
+
     //Personal Area Post Method
     @RequestMapping(value = "/personal_area", method = RequestMethod.POST)
     public String personalAreaPost(final Model model) throws ServiceException {
-
         return "redirect:/personal_area";
     }
+
     //Store Area Get Method
     @RequestMapping(value = "/store_area", method = RequestMethod.GET)
     public String storeAreaGet(final Model model) {
         return "home/store_area";
     }
+
     //Store Area Post Method
     @RequestMapping(value = "/store_area", method = RequestMethod.POST)
     public String storeAreaPost(final Model model) {
