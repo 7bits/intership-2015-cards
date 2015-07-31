263834159399e7a4e2fd79c72d8433cb313d1793
Deamor <deamor76@gmail.com>
2015-07-31 14:03:02 +0600

    Fix search script, fix form styles for store

--- src/main/resources/public/resources/scripts/search.js
+++ src/main/resources/public/resources/scripts/search.js
@@ -2,14 +2,14 @@ window.onload = function(){
 
 $('#send-search').on('input', function(){
     var text = $(this).val();
-    $('#send_tab .accordion-heading').show();    
-    $('#send_tab .accordion-heading:not(:contains(' + text + '))').hide();
+    $('#send_tab .accordion-group.discount').show();
+    $('#send_tab .accordion-heading .heading:not(:contains(' + text + '))').parents(".accordion-group.discount").hide();
 });
 
 $('#use-search').on('input', function(){
     var text = $(this).val();
-    $('#use_tab .accordion-heading').show();    
-    $('#use_tab .accordion-heading:not(:contains(' + text + '))').hide();
+    $('#use_tab .accordion-group.discount').show();
+    $('#use_tab .accordion-heading .heading:not(:contains(' + text + '))').parents(".accordion-group.discount").hide();
 });
 
 }
