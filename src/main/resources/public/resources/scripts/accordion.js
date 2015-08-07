56afb50e48088fb68d9c88dccd5afeba5ebfb8d6
taro <dietzvv@gmail.com>
2015-08-07 14:05:11 +0600

    fix ajax on forms

---
+++ src/main/resources/public/resources/scripts/accordion.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.js-send').click('submit', '#js-send-form' ,function(e){
+        e.preventDefault();
+        var token = $("meta[name='_csrf']").attr("content");
+        var header = $("meta[name='_csrf_header']").attr("content");
+        var headers = {};
+        headers[header] = token;
+        doAjaxPost(e.target, headers);
+    })
+});
--- src/main/resources/public/resources/scripts/useDiscountValidation.js
+++ src/main/resources/public/resources/scripts/useDiscountValidation.js
@@ -1,31 +1,28 @@
-$(document).ready(function(){
-    $('.js-use').on('submit', '#js-use-form' ,function(e){
-        e.preventDefault();
-        var token = $("meta[name='_csrf']").attr("content");
-        var header = $("meta[name='_csrf_header']").attr("content");
-        var headers = {};
-        headers[header] = token;
-        doAjaxPost(e.target, headers);
-    })
-});
-
-function doAjaxPost(data, headers) {
+function doAjaxPostUse() {
+var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
+    var headers = {};
+    headers[header] = token;
+var key = $('#keyUse').val();
     $.ajax({
         type: "POST",
         url: "/use_discount",
-        data: $(data).serialize(),
+        data: "key=" + key,
         headers: headers,
         success: function(response){
-            $('.remove-error-message').html('');
+            $('.errors').html('');
+            $('.infoBlock').html('');
+            $('.form-control').removeClass("red-error");
             if(response.status =="FAIL") {
                 for (var p in response.result) {
                     if (response.result.hasOwnProperty(p)) {
-                        $('#' + p).html(response.result[p]);
+                        $('.'+p+'_use_input').addClass("red-error");
+                        $('#' + p + "Error").html(response.result[p]);
                     }
                 }
             }
             else {
-                //$('#info').html("<h4>"+"Скидка успешно добавлена!"+"</h4>");
+                $('.infoBlock').html("Скидка успешно добавлена!");
             }
         },
         error: function(e){
