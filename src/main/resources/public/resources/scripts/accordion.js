eabb6ab57f40d7981fb6065487910dce89fb7013
taro <dietzvv@gmail.com>
2015-08-20 16:02:36 +0600

    add adaptive front end on  header and subpages + fix accordion opening

--- src/main/resources/public/resources/scripts/accordion.js
+++ src/main/resources/public/resources/scripts/accordion.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     $('.tab-content').on("click", "div.ellement", function(){
     	var ellementId = '#' + this.id + '_sub';
     	if( $(ellementId).is(':visible') ) {
-            $("#accordion_arrow_1").rotateRight(180);
     		$(this).removeClass('head-open');
     		$(ellementId).slideUp();
     	} else {
