256297a5dfdce136e7ac26512153a58ff9aed5d4
Deamor <deamor76@gmail.com>
2015-08-21 15:20:44 +0600

    Fix search

--- src/main/resources/public/resources/scripts/search.js
+++ src/main/resources/public/resources/scripts/search.js
@@ -2,14 +2,28 @@ window.onload = function(){
 
 $('#send-search').on('input', function(){
     var text = $(this).val();
-    $('#send_tab .accordion-group.discount').show();
-    $('#send_tab .accordion-heading .heading:not(:contains(' + text + '))').parents(".accordion-group.discount").hide();
+    $('#send_tab .ellement').show();
+    if($('#send_tab .ellement:not(:contains(' + text + '))').css("display")!=='none' && $('#send_tab .ellement:not(:contains(' + text + '))').next().css("display")!=='none') {
+        $('#send_tab .ellement').next().show();
+    }
+    $('#send_tab .ellement:not(:contains(' + text + '))').hide();
+    if($('#send_tab .ellement:not(:contains(' + text + '))').css("display")=='none') {
+        $('#send_tab .ellement:not(:contains(' + text + '))').next().hide();
+    }
+
+
 });
 
 $('#use-search').on('input', function(){
     var text = $(this).val();
-    $('#use_tab .accordion-group.discount').show();
-    $('#use_tab .accordion-heading .heading:not(:contains(' + text + '))').parents(".accordion-group.discount").hide();
+    $('#use_tab .ellement').show();
+    if($('#use_tab .ellement:not(:contains(' + text + '))').css("display")!=='none' && $('#use_tab .ellement:not(:contains(' + text + '))').next().css("display")!=='none') {
+        $('#use_tab .ellement').next().show();
+    }
+    $('#use_tab .ellement:not(:contains(' + text + '))').hide();
+    if($('#use_tab .ellement:not(:contains(' + text + '))').css("display")=='none') {
+        $('#use_tab .ellement:not(:contains(' + text + '))').next().hide();
+    }
 });
 
 }
