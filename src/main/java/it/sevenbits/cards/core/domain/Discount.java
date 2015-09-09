d42cedbd764c20fcb5f96c6651722367a9cc9df9
Kuzmin Dmitry <kuzmindmitry1994@gmail.com>
2015-09-09 17:42:58 +0600

    Edit Discount class for current tables in batabase

--- src/main/java/it/sevenbits/cards/core/domain/Discount.java
+++ src/main/java/it/sevenbits/cards/core/domain/Discount.java
@@ -1,20 +1,18 @@
 package it.sevenbits.cards.core.domain;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
 
 public class Discount implements Serializable {
     private Long id;
     private String key;
-    private String uin;
     private Boolean isHidden;
-    private String userId;
-    private String storeName;
-    private String description;
-    private int percent;
-    private String storeImage;
-    private int backerPercent;
-    private String backerUserId;
     private String email;
+    private String backerEmail;
+    private Long campaignId;
+    private Boolean deleted;
+    private String hash;
+    private Timestamp createdAt;
 
     public Long getId() {
         return id;
@@ -32,76 +30,12 @@ public class Discount implements Serializable {
         this.key = key;
     }
 
-    public String getUin() {
-        return uin;
-    }
-
-    public void setUin(String uin) {
-        this.uin = uin;
-    }
-
     public Boolean getIsHidden() {
         return isHidden;
     }
 
     public void setIsHidden(Boolean isHidden) {
         this.isHidden = isHidden;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getStoreName() {
-        return storeName;
-    }
-
-    public void setStoreName(String storeName) {
-        this.storeName = storeName;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getPercent() {
-        return percent;
-    }
-
-    public void setPercent(int percent) {
-        this.percent = percent;
-    }
-
-    public String getStoreImage() {
-        return storeImage;
-    }
-
-    public void setStoreImage(String storeImage) {
-        this.storeImage = storeImage;
-    }
-
-    public int getBackerPercent() {
-        return backerPercent;
-    }
-
-    public void setBackerPercent(int backerPercent) {
-        this.backerPercent = backerPercent;
-    }
-
-    public String getBackerUserId() {
-        return backerUserId;
-    }
-
-    public void setBackerUserId(String backerUserId) {
-        this.backerUserId = backerUserId;
     }
 
     public String getEmail() {
@@ -112,10 +46,50 @@ public class Discount implements Serializable {
         this.email = email;
     }
 
+    public String getBackerEmail() {
+        return backerEmail;
+    }
+
+    public void setBackerEmail(String backerEmail) {
+        this.backerEmail = backerEmail;
+    }
+
+    public Long getCampaignId() {
+        return campaignId;
+    }
+
+    public void setCampaignId(Long campaignId) {
+        this.campaignId = campaignId;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
     @Override
     public String toString() {
-        return String.format("Discount[id=%d, key=%s, uid=%s, isHidden=%b, userId=%s, storeName=%s, description=%s, percent=%d, backerPercent=%d, backerUserId=%s, email=%s]",
-                id, key, uin, isHidden, userId, storeName, description, percent, backerPercent, backerUserId, email);
+        return String.format("Discount[id=%d, key=%s, isHidden=%b, email=%s, backerEmail=%s, campaignId=%d, deleted=%b, hash=%s, createdAt=%s]",
+                id, key, isHidden, email, backerEmail, campaignId, deleted, hash, createdAt.toString());
     }
 }
 
